030329a0f86680a923507da8c67c709f1ab80316
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-19 12:48:54 +0530

    layout added in blog detail page

---
+++ pages/blog/[slug].js
@@ -0,0 +1,16 @@
+// components
+import Layout from "../../components/Layout";
+
+function BlogDetail() {
+  return (
+    <>
+      <Layout>
+        <main>
+          <h1>Blog detail</h1>
+        </main>
+      </Layout>
+    </>
+  );
+}
+
+export default BlogDetail;
