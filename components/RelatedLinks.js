2a0ed897167d81892083054ab2498a966bf04a85
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-20 10:58:26 +0530

    related link component implemented

---
+++ components/RelatedLinks.js
@@ -0,0 +1,28 @@
+function RelatedLinks(props) {
+  const related_links = JSON.parse(JSON.stringify(props.blog.related_links));
+  return (
+    <aside className="side-bar">
+      <h2>Related Links</h2>
+      <div className="related-link-container">
+        {related_links.map((link, index) => {
+          return (
+            <div
+              className="related-link"
+              //   onClick={this.props.renderNewBlog}
+              id={link.uid}
+              key={link.uid + index}
+            >
+              <img
+                src="https://github.com/ialtafshaikh/static-files/raw/master/dummp-image.jpg"
+                alt=""
+              />
+              <p className="link-title">{link.uid}</p>
+            </div>
+          );
+        })}
+      </div>
+    </aside>
+  );
+}
+
+export default RelatedLinks;
