d05e3e57b3ae2980c350b6567112b2a1bbdc7618
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-20 10:51:44 +0530

    blog detail page configured with slug data

--- contentstack/queries/getAllEnteries.js
+++ contentstack/queries/getAllEnteries.js
@@ -1,7 +1,24 @@
 import Stack from "../Stack";
 
-export default function getAllEnteries(contentType) {
+export default function getAllEnteries(contentType, condition) {
   const Query = Stack.ContentType(contentType).Query();
+
+  if (typeof condition != "undefined") {
+    return Query.where(condition.prop, condition.value)
+      .toJSON()
+      .find()
+      .then(
+        function success(result) {
+          // console.log(result[0]); // list of objects of employee details
+          return result[0];
+        },
+        function error(err) {
+          // err object
+          console.log(err);
+          return err;
+        }
+      );
+  }
   return Query.toJSON()
     .find()
     .then(
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,10 +1,21 @@
+import { useRouter } from "next/router";
+
 // components
 import Layout from "../../components/Layout";
+import getAllEnteries from "../../contentstack/queries/getAllEnteries";
 
-function BlogDetail() {
-  return (
+function BlogDetail(props) {
+  const router = useRouter();
+
+  return router.isFallback ? (
+    <p>Loading..</p>
+  ) : (
     <>
-      <Layout>
+      <Layout
+        title={props.blog.title}
+        NavigationContent={{ navbarContent: props.navbar }}
+        FooterContent={{ footerContent: props.footer }}
+      >
         <main>
           <h1>Blog detail</h1>
         </main>
@@ -12,5 +23,36 @@ function BlogDetail() {
     </>
   );
 }
+
+export const getStaticProps = async (context) => {
+  const result = await getAllEnteries("blogs", {
+    prop: "url",
+    value: "/" + context.params.slug,
+  });
+  const navbarData = await getAllEnteries("navbar");
+  const footerData = await getAllEnteries("footer");
+
+  return {
+    props: {
+      blog: { ...result[0] },
+      navbar: { ...navbarData[0] },
+      footer: { ...footerData[0] },
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const result = await getAllEnteries("blogs");
+  let paths = result.map((blog) => ({
+    params: {
+      slug: blog.url.slice(1),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
 
 export default BlogDetail;
